fdd6df0e23880d1a8cf9f7d3c64d3a2d29db13b3
DawidPludowski <dawid.pludowski@gmail.com>
2024-06-01 11:22:32 +0200

    finish streamlit app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import json
+import time
+import webbrowser
 from functools import partial
 
 import requests
@@ -6,12 +8,13 @@ import streamlit as st
 
 site = "https://ragapi-hw2k5v4d7q-uc.a.run.app"
 
-st.session_state.TOKEN = "<TMP_TOKEN>"
+st.set_page_config(page_title="RAG chat")
+st.session_state.TOKEN = None
 
 history_containers: dict[int, st.container] = {}
 
 
-def get_response(message: str) -> str:
+def get_response(message: str, token) -> str:
     directory: str = "/chatbot/message/new"
 
     response = requests.post(
@@ -20,21 +23,23 @@ def get_response(message: str) -> str:
         headers={
             "accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + st.session_state.TOKEN,
+            "Authorization": "Bearer " + token,
         },
     )
 
     return response.json()["text"]
 
 
-def _get_user_history():
+@st.cache_resource
+def _get_user_history(token):
+
     directory = "/chatbot/message/fetchAll"
     response = requests.get(
         site + directory,
         headers={
             "accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + st.session_state.TOKEN,
+            "Authorization": "Bearer " + token,
         },
     )
 
@@ -42,89 +47,225 @@ def _get_user_history():
 
 
 def _setup_initial_state():
-    st.session_state.is_logged = False
+    first_message = (
+        "How can I help you?"
+        if st.session_state.is_logged
+        else "You have no token! Please obtain one by logging via Google (button on the left)."
+    )
     if "messages" not in st.session_state.keys():
         st.session_state.messages = [
-            {"role": "assistant", "content": "How may I help you?"}
+            {"role": "assistant", "content": first_message}
         ]
 
 
 def _setup_layout():
 
-    st.set_page_config(page_title="RAG chat")
-
     with st.sidebar:
-        st.title("RAG chat history")
-        st.button("Login with Google", key="google-log", on_click=_login)
+        st.markdown(
+            """
+        <style>
+        .really-big-font {
+            font-size:60px !important;
+        }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            '<p class="really-big-font">RAG chat</p>', unsafe_allow_html=True
+        )
+
+        if not st.session_state.is_logged:
+            st.button(
+                "Login with Google",
+                key="google-log",
+                on_click=_redirect_to_google,
+            )
+
+        if st.session_state.TOKEN is not None:
+            st.button(
+                "Delete all history",
+                key="delete-history",
+                on_click=_delete_all_history,
+            )
 
 
-def _login():
+def _delete_all_history():
+
+    keys = list(history_containers.keys())
+    for idx in keys:
+        _delete_history_element(idx, st.session_state.TOKEN)
+
+
+def _sign_out():
     raise NotImplementedError
+
+
+def _redirect_to_google():
+
+    directory = "/login"
+
+    response = requests.get(
+        site + directory, headers={"accept": "application/json"}
+    )
+
+    authorization_url = response.json()["url"]
+    webbrowser.open(authorization_url)
+
+
+@st.cache_resource
+def _get_token(code):
+
+    directory = "/auth"
+
+    response = requests.get(
+        site + directory + f"?code={code}",
+        headers={
+            "accept": "application/json",
+        },
+    )
+
+    token = response.json()["token_json"]["id_token"]
     st.session_state.is_logged = True
-    st.session_state.TOKEN = "..."
+
+    return token
 
 
-def _delete_history_element(id_: int):
+def _delete_history_element(id_: int, token):
 
     del history_containers[id_]
 
-    _post_delete_history(id_)
+    ids = list(map(lambda item: item["id"], st.session_state.history))
+    history_item = ids.index(id_)
+
+    del st.session_state.history[history_item]
+
+    if token is not None:
+        _post_delete_history(id_, token)
 
 
-def _post_delete_history(id_: int):
+def _post_delete_history(id_: int, token):
+
+    if token is None:
+        return
 
     directory = f"/chatbot/message/{id_}/delete"
     _ = requests.delete(
         site + directory,
         headers={
             "accept": "application/json",
-            "Authorization": "Bearer " + st.session_state.TOKEN,
+            "Authorization": "Bearer " + token,
         },
     )
 
 
-def _load_history():
+def _add_to_history(prompt: str, response: str, username: str, id_: int):
 
-    history = _get_user_history()
+    with st.sidebar:
+        new_container = st.container(border=True)
+        with new_container:
+            st.markdown(f"**{username}**: {prompt}")
+            st.markdown(f"**Response**: {response}")
+            st.button(
+                "🗑️",
+                key=id_,
+                on_click=partial(_delete_history_element, id_, None),
+            )
 
-    if len(history) == 0:
-        with st.sidebar:
-            with st.container(border=True):
-                st.markdown("The history of the chat is empty! 😥.")
+    history_containers[id_] = new_container
+    st.session_state.history.append(
+        {"prompt": prompt, "response": response, "id": id_}
+    )
+
+
+def _load_history(token):
+
+    with st.sidebar:
+        st.markdown(
+            """
+        <style>
+        .big-font {
+            font-size:30px !important;
+        }
+        </style>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            '<p class="big-font">Chat History</p>', unsafe_allow_html=True
+        )
+
+    if token is None:
+        return
+
+    history = st.session_state.history
+
+    if isinstance(history, dict) and len(history_containers) == 0:
         return
 
     with st.sidebar:
         for history_item in history:
-            full_username = history_item["username"]
             id_ = history_item["id"]
-            try:
-                at_index = full_username.index("@")
-            except ValueError:
-                at_index = len(full_username)
-            username = full_username[:at_index]
+
             new_container = st.container(border=True)
             with new_container:
-                st.markdown(f"**{username}**: {history_item['prompt']}")
+                st.markdown(f"**You**: {history_item['prompt']}")
                 st.markdown(f"**Response**: {history_item['response']}")
                 st.button(
                     "🗑️",
                     key=id_,
-                    on_click=partial(_delete_history_element, id_),
+                    on_click=partial(_delete_history_element, id_, token),
                 )
 
             history_containers[id_] = new_container
 
 
+def _try_get_token():
+
+    if st.session_state.TOKEN is not None:
+        return st.session_state.TOKEN
+
+    with st.spinner("Processing..."):
+        time.sleep(0.5)
+
+        if "code" in list(st.query_params.keys()):
+            token = _get_token(st.query_params["code"])
+        else:
+            token = None
+        return token
+
+
 def main():
+
+    local_couter = -1
+
+    st.session_state.is_logged = (
+        True if "code" in list(st.query_params.keys()) is not None else False
+    )
+
+    if st.session_state.TOKEN is None:
+        token = _try_get_token()
+        st.session_state.TOKEN = token
+
+    if token:
+        history = _get_user_history(token)
+        if isinstance(history, dict):
+            st.session_state.history = []
+        st.session_state.history = history
+    else:
+        st.session_state.history = []
+
     _setup_initial_state()
     _setup_layout()
-    _load_history()
+    _load_history(token)
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
-    if prompt := st.chat_input():
+    if prompt := st.chat_input(disabled=not st.session_state.is_logged):
         if prompt is not None:
 
             # User-provided prompt
@@ -139,10 +280,15 @@ def main():
                 with st.chat_message("assistant"):
 
                     with st.spinner("Thinking..."):
-                        response = get_response(prompt)
+                        response = get_response(prompt, token)
                         st.write(response)
                 chatbot_message = {"role": "assistant", "content": response}
                 st.session_state.messages.append(chatbot_message)
+
+                _add_to_history(
+                    prompt, chatbot_message["content"], "You", local_couter
+                )
+                local_couter -= 1
 
 
 if __name__ == "__main__":
